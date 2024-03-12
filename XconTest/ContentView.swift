90544650a0095f227b19c855f2c2d7eb9182c21e
GH <BoneAsh@iCloud.com>
2024-03-12 10:29:17 +0800

    Initial Commit

---
+++ XconTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XconTest
+//
+//  Created by GH on 2024/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XconTest/XconTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XconTestApp.swift
+//  XconTest
+//
+//  Created by GH on 2024/3/12.
+//
+
+import SwiftUI
+
+@main
+struct XconTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
