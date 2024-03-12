26fd89f54e9dcb0e79c45ca0c443f47eda9089a2
GH <BoneAsh@iCloud.com>
2024-03-12 14:51:37 +0800

    test3

--- XconTest/ContentView.swift
+++ XconTest/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("check!")
+<<<<<<< Updated upstream
+            Text("check!!")
+=======
+            Text("merge test")
+>>>>>>> Stashed changes
         }
         .padding()
     }
