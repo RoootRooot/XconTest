528271a1c0dac8a4ecf4b7b3ab1e49829fc879f8
GH <BoneAsh@iCloud.com>
2024-03-12 13:43:00 +0800

    Bitbucket test

--- XconTest/ContentView.swift
+++ XconTest/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Analyze Test")
+            Text("Analyze Test!")
         }
         .padding()
     }
